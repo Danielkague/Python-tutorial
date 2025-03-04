7b5f29cd3c42f3ec832c007b50269c5089cc921b
Danielkague <daniel.kague@gmail.com>
2025-03-04 12:31:34 +0300

    python navigation

---
+++ Intro2.py
@@ -0,0 +1,22 @@
+#interger(whole number)
+age = 25
+#float(decimal numbers)
+height = 5.9
+#string(text)
+name = 'Alice'
+#boolean(true/false)
+is_student = True
+
+# check data types
+print(type(age)) #Output: <class 'int'>
+print(type(height)) #Output: <class 'int'>
+print(type(name)) #Output: <class 'int'>
+print(type(is_student)) #Output: <class 'int'>
+
+#Dynamic typing:
+age = 20
+print (age) #Output:20
+age = "Now I am twenty"
+print(age) #Output:string
+
+
---
+++ modulesx.py
@@ -0,0 +1,5 @@
+import math
+
+# Using the math module to calculate the square root
+print(math.sqrt(16))  # Outputs: 4.0
+print(math.sqrt(81))
